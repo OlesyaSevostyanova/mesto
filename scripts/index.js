163eb5b69126b7ebd8d3f2c669b6f48bc5de13e1
Olesya Sevostyanova <dagassa@yandex.ru>
2023-02-06 02:45:33 +0300

    initialCards по шаблону через JavaScript

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,54 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditPopup = document.querySelector(".popup");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 const profilePopupForm = document.querySelector(".profile-edit-form");
 const profilePopupCloseButton = document.querySelector(".popup__close-button");
+
+const elementList = document.querySelector(".elements__list");
+const elementTemplate = document.querySelector("#element-template").content;
+
+function createCard (card) {
+  const newCard =  elementTemplate.querySelector(".element").cloneNode(true);
+   
+  const elementImage = newCard.querySelector(".element__image");
+  elementImage.src = card.link
+  elementImage.alt = card.name
+
+  newCard.querySelector(".element__name").textContent = card.name;
+
+  elementList.prepend(newCard);
+}
+
+initialCards.forEach(card => createCard(card));
+
 
 function openProfileEditPopup () {
   renderPopup();
