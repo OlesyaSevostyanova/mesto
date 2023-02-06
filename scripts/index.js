a1cb6876a2f8a6898325c0c76557ab300e9b3686
Olesya Sevostyanova <dagassa@yandex.ru>
2023-02-06 16:14:13 +0300

    переделаны попаппы с инпутами, добавлен второй попап 'Создать место'

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// стартовые карточки
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -25,62 +27,93 @@ const initialCards = [
   }
 ];
 
-const profileEditButton = document.querySelector(".profile__edit-button");
-const profileEditPopup = document.querySelector(".popup");
-const profileName = document.querySelector(".profile__name");
-const profileDescription = document.querySelector(".profile__description");
-const profilePopupForm = document.querySelector(".profile-edit-form");
-const profilePopupCloseButton = document.querySelector(".popup__close-button");
+// добавление / удаление карточек
 
-const elementList = document.querySelector(".elements__list");
+const elementsList = document.querySelector(".elements__list");
 const elementTemplate = document.querySelector("#element-template").content;
 
-function cardLikeToggle (evt) {
+function cardLikeToggle(evt) {
   evt.target.classList.toggle("element__like-button_active");
 }
 
-function deleteCard (evt) {
+function deleteCard(evt) {
   evt.target.parentElement.remove();
 }
 
-function createCard (card) {
-  const newCard =  elementTemplate.querySelector(".element").cloneNode(true);
-   
-  const elementImage = newCard.querySelector(".element__image");
-  elementImage.src = card.link
-  elementImage.alt = card.name
+function createCard(name, link) {
+  const newCard = elementTemplate.querySelector(".element").cloneNode(true);
 
-  newCard.querySelector(".element__name").textContent = card.name;
+  const elementImage = newCard.querySelector(".element__image");
+  elementImage.src = link;
+  elementImage.alt = name;
+
+  newCard.querySelector(".element__name").textContent = name;
 
   newCard.querySelector(".element__like-button").addEventListener('click', cardLikeToggle);
   newCard.querySelector(".element__trash-button").addEventListener('click', deleteCard);
 
-  elementList.prepend(newCard);
+  elementsList.prepend(newCard);
 }
 
-initialCards.forEach(card => createCard(card));
+// Заполняем стартовые карточки
 
-function openProfileEditPopup () {
-  renderPopup();
+initialCards.forEach(card => createCard(card.name, card.link));
+
+
+// общая функция для всех попапов
+
+function closePopup(evt) {
+  evt.target.closest(".popup").classList.remove("popup_opened");
+}
+
+
+// попап "Создать место"
+
+const addCardButton = document.querySelector(".profile__add-button");
+const addCardForm = document.querySelector("#add-card-form");
+const addCardPopup = addCardForm.closest(".popup");
+const addCardPopupCloseButton = addCardPopup.querySelector(".popup__close-button");
+
+function openAddCardPopup() {
+  addCardPopup.classList.add("popup_opened");
+}
+
+function onAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  createCard(
+    addCardForm.elements["name"].value,
+    addCardForm.elements["link"].value
+  )
+  closePopup(evt);
+}
+
+addCardButton.addEventListener('click', openAddCardPopup);
+addCardPopupCloseButton.addEventListener('click', closePopup);
+addCardForm.addEventListener('submit', onAddCardFormSubmit);
+
+
+// попап "Редактировать профиль"
+
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditForm = document.querySelector("#profile-edit-form");
+const profileEditPopup = profileEditForm.closest(".popup");
+const profileEditPopupCloseButton = profileEditPopup.querySelector(".popup__close-button");
+
+function openProfileEditPopup() {
+  profileEditForm.elements["name"].value = profileName.textContent;
+  profileEditForm.elements["description"].value = profileDescription.textContent;
   profileEditPopup.classList.add("popup_opened");
 }
 
-function closeProfileEditPopup () {
-  profileEditPopup.classList.remove("popup_opened");
-}
-
-function renderPopup () {
-  profilePopupForm.elements["name"].value = profileName.textContent;
-  profilePopupForm.elements["description"].value = profileDescription.textContent;
-}
-
-function onProfileFormSubmit (evt) {
+function onProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = profilePopupForm.elements["name"].value;
-  profileDescription.textContent = profilePopupForm.elements["description"].value;
-  closeProfileEditPopup();
+  profileName.textContent = profileEditForm.elements["name"].value;
+  profileDescription.textContent = profileEditForm.elements["description"].value;
+  closePopup(evt);
 }
 
 profileEditButton.addEventListener('click', openProfileEditPopup);
-profilePopupCloseButton.addEventListener('click', closeProfileEditPopup);
-profilePopupForm.addEventListener('submit', onProfileFormSubmit);
+profileEditPopupCloseButton.addEventListener('click', closePopup);
+profileEditForm.addEventListener('submit', onProfileFormSubmit);
